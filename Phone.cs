ed447271c68a27f8a5d91dfd76b782cc6bc14f40
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-01 15:03:28 +1200

    create form with different fields, grid area and buttons

--- Phone.cs
+++ Phone.cs
@@ -16,5 +16,40 @@ namespace TelephoneDiary
         {
             InitializeComponent();
         }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
